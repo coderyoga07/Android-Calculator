db161fbd911eea9c9b2048a3bcb93cfc2ad535c5
coderyoga07 <coderyoga07@gmail.com>
2026-06-26 22:43:58 +0530

    Final fallback: V2

--- android/app/src/main/kotlin/com/vibe/calculator/MainActivity.kt
+++ android/app/src/main/kotlin/com/vibe/calculator/MainActivity.kt
@@ -3,5 +3,4 @@ package com.vibe.calculator
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-    // Upgraded to v2 embedding: io.flutter.embedding.android.FlutterActivity
 }
