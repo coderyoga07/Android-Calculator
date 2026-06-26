a8e84bfe74af8e8f032e3bf3bd54c14c1fdfbb15
coderyoga07 <coderyoga07@gmail.com>
2026-06-26 20:00:14 +0530

    Migrated to Android v2 embedding for Codemagic

--- android/app/src/main/kotlin/com/vibe/calculator/MainActivity.kt
+++ android/app/src/main/kotlin/com/vibe/calculator/MainActivity.kt
@@ -3,4 +3,5 @@ package com.vibe.calculator
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    // Upgraded to v2 embedding: io.flutter.embedding.android.FlutterActivity
 }
